349041a98734d678389d41b874d9518dd5a907ee
Edwin pinzón <113047760+edwinpinzon@users.noreply.github.com>
2024-05-03 19:07:22 -0500

    CambiosCargarEscenasFinales

--- Assets/UI/ScriptUI/LevelLoader.cs
+++ Assets/UI/ScriptUI/LevelLoader.cs
@@ -3,14 +3,14 @@ using UnityEngine.SceneManagement;
 
 public static class LevelLoader
 {
-    public static string nextLelevel;
-    public static void LoadLelevel(string name)
+    public static string nextLevel;
+    public static string loadingScene = "SceneLoading"; // Escena de carga por defecto
+
+    public static void LoadLevel(string name, string loadingSceneName = null)
     {
-        nextLelevel=name;
-        SceneManager.LoadScene("SceneLoading");
-        
+        nextLevel = name;
+        if (!string.IsNullOrEmpty(loadingSceneName))
+            loadingScene = loadingSceneName; // Si se especifica una escena de carga, usarla
+        SceneManager.LoadScene(loadingScene);
     }
-
-
-
 }
